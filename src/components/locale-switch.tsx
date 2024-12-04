ff25bd24dfcb78e92ffa560b8fde4b5e742d5e0f
Cierra_Runis <2864283875@qq.com>
2024-12-04 23:03:17 +0800

    🔧Fix logic for mobile device

--- src/components/locale-switch.tsx
+++ src/components/locale-switch.tsx
@@ -2,47 +2,41 @@
 
 import { redirect, routing, usePathname } from '@/i18n/routing';
 import { LanguageIcon } from '@heroicons/react/24/outline';
-import { Button, Listbox, ListboxItem, Tooltip } from '@nextui-org/react';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@nextui-org/react';
 import { useLocale, useTranslations } from 'next-intl';
-import { useState } from 'react';
 
 export default function LocaleSwitch() {
   const locale = useLocale();
   const pathname = usePathname();
   const t = useTranslations();
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Tooltip
-      isOpen={isOpen}
-      onOpenChange={(open) => setIsOpen(open)}
-      content={
-        <Listbox
-          aria-label='Actions'
-          selectionMode='single'
-          selectedKeys={[locale]}
-        >
-          {routing.locales.map((key) => (
-            <ListboxItem
-              key={key}
-              onClick={() => {
-                setIsOpen(false);
-                redirect({ locale: key, href: { pathname: pathname } });
-              }}
-            >
-              {t(`locales.${key}`)}
-            </ListboxItem>
-          ))}
-        </Listbox>
-      }
-    >
-      <Button
-        size='sm'
-        variant='light'
-        startContent={<LanguageIcon className='text-default-500 w-5' />}
-        isIconOnly
-        onClick={() => setIsOpen(true)}
-      />
-    </Tooltip>
+    <Dropdown>
+      <DropdownTrigger>
+        <Button
+          size='sm'
+          variant='light'
+          startContent={<LanguageIcon className='text-default-500 w-5' />}
+          isIconOnly
+        />
+      </DropdownTrigger>
+      <DropdownMenu
+        selectedKeys={[locale]}
+        selectionMode='single'
+        onAction={(key) =>
+          redirect({ locale: key as string, href: { pathname: pathname } })
+        }
+      >
+        {routing.locales.map((key) => (
+          <DropdownItem key={key} title={t(`locales.${key}`)} />
+        ))}
+      </DropdownMenu>
+    </Dropdown>
   );
 }
