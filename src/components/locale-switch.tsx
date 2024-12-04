d4fed77126393e85b3805d9c1eef6675d3f67694
Cierra_Runis <2864283875@qq.com>
2024-12-04 22:39:49 +0800

    🔧Fix logic for mobile device

--- src/components/locale-switch.tsx
+++ src/components/locale-switch.tsx
@@ -4,13 +4,18 @@ import { redirect, routing, usePathname } from '@/i18n/routing';
 import { LanguageIcon } from '@heroicons/react/24/outline';
 import { Button, Listbox, ListboxItem, Tooltip } from '@nextui-org/react';
 import { useLocale, useTranslations } from 'next-intl';
+import { useState } from 'react';
 
 export default function LocaleSwitch() {
   const locale = useLocale();
   const pathname = usePathname();
   const t = useTranslations();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Tooltip
+      isOpen={isOpen}
+      onOpenChange={(open) => setIsOpen(open)}
       content={
         <Listbox
           aria-label='Actions'
@@ -21,6 +26,7 @@ export default function LocaleSwitch() {
             <ListboxItem
               key={key}
               onClick={() => {
+                setIsOpen(false);
                 redirect({ locale: key, href: { pathname: pathname } });
               }}
             >
@@ -35,6 +41,7 @@ export default function LocaleSwitch() {
         variant='light'
         startContent={<LanguageIcon className='text-default-500 w-5' />}
         isIconOnly
+        onClick={() => setIsOpen(true)}
       />
     </Tooltip>
   );
