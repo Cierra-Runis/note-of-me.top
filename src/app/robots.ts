e984a14c7af54842f44ff263e574f35cae067c47
Cierra-Runis <2864283875@qq.com>
2024-12-09 01:10:07 +0800

    🔧 Fix error in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: '/sitemap.xml',
+    sitemap: `${host}/sitemap.xml`,
     host: host,
   };
 }
