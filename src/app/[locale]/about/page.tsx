a37be0daadf6f81edc17cfbfccdc99034400e3c8
Cierra_Runis <2864283875@qq.com>
2024-12-09 18:35:58 +0800

    📖Refactor technical stack buttons

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -21,6 +21,81 @@ import {
 import { Button } from '@nextui-org/button';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+import { ReactElement } from 'react';
+
+const technicalStack: {
+  name: string;
+  url: string;
+  icon: ReactElement;
+}[] = [
+  {
+    name: 'React',
+    url: 'https://react.dev/',
+    icon: <SiReact className='w-5 text-default-500' color={SiReactHex} />,
+  },
+  {
+    name: 'Next.js',
+    url: 'https://nextjs.org/',
+    icon: <SiNextdotjs className='w-5 text-default-500' />,
+  },
+  {
+    name: 'Vercel',
+    url: 'https://vercel.com/',
+    icon: <SiVercel className='w-5 text-default-500' />,
+  },
+  {
+    name: 'TailwindCSS',
+    url: 'https://tailwindcss.com/',
+    icon: (
+      <SiTailwindcss
+        className='w-5 text-default-500'
+        color={SiTailwindcssHex}
+      />
+    ),
+  },
+  {
+    name: 'NextUI',
+    url: 'https://nextui.org/',
+    icon: <SiNextui className='w-5 text-default-500' />,
+  },
+  {
+    name: 'Yarn',
+    url: 'https://yarnpkg.com/',
+    icon: <SiYarn className='w-5 text-default-500' color={SiYarnHex} />,
+  },
+  {
+    name: 'MDX',
+    url: 'https://mdxjs.com/',
+    icon: <SiMdx className='w-5 text-default-500' />,
+  },
+  {
+    name: 'TypeScript',
+    url: 'https://www.typescriptlang.org/',
+    icon: (
+      <SiTypescript className='w-5 text-default-500' color={SiTypescriptHex} />
+    ),
+  },
+  {
+    name: 'EsLint',
+    url: 'https://eslint.org/',
+    icon: <SiEslint className='w-5 text-default-500' color={SiEslintHex} />,
+  },
+  {
+    name: 'Git',
+    url: 'https://git-scm.com/',
+    icon: <SiGit className='w-5 text-default-500' color={SiGitHex} />,
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/',
+    icon: <SiGithub className='w-5 text-default-500' />,
+  },
+  {
+    name: 'Simple Icons',
+    url: 'https://simpleicons.org/',
+    icon: <SiSimpleicons className='w-5 text-default-500' />,
+  },
+];
 
 export default function AboutPage() {
   const t = useTranslations();
@@ -42,135 +117,18 @@ export default function AboutPage() {
             {t('whatAreWeUsingNow')}
           </h2>
           <div className='mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5'>
-            <Button
-              href='https://react.dev/'
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiReact className='w-5 text-default-500' color={SiReactHex} />
-              }
-              as={Link}
-              variant='light'
-            >
-              React
-            </Button>
-            <Button
-              href='https://nextjs.org/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiNextdotjs className='w-5 text-default-500' />}
-              variant='light'
-            >
-              Next.js
-            </Button>
-            <Button
-              href='https://vercel.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiVercel className='w-5 text-default-500' />}
-              variant='light'
-            >
-              Vercel
-            </Button>
-            <Button
-              href='https://tailwindcss.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiTailwindcss
-                  className='w-5 text-default-500'
-                  color={SiTailwindcssHex}
-                />
-              }
-              variant='light'
-            >
-              TailwindCSS
-            </Button>
-            <Button
-              href='https://nextui.org/'
-              as={Link}
-              className='f col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiNextui className='w-5 text-default-500' />}
-              variant='light'
-            >
-              NextUI
-            </Button>
-            <Button
-              href='https://yarnpkg.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiYarn className='w-5 text-default-500' color={SiYarnHex} />
-              }
-              variant='light'
-            >
-              Yarn
-            </Button>
-            <Button
-              href='https://mdxjs.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiMdx className='w-5 text-default-500' />}
-              variant='light'
-            >
-              MDX
-            </Button>
-            <Button
-              href='https://www.typescriptlang.org/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiTypescript
-                  className='w-5 text-default-500'
-                  color={SiTypescriptHex}
-                />
-              }
-              variant='light'
-            >
-              TypeScript
-            </Button>
-            <Button
-              href='https://eslint.org/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiEslint
-                  className='w-5 text-default-500'
-                  color={SiEslintHex}
-                />
-              }
-              variant='light'
-            >
-              EsLint
-            </Button>
-            <Button
-              href='https://git-scm.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={
-                <SiGit className='w-5 text-default-500' color={SiGitHex} />
-              }
-              variant='light'
-            >
-              Git
-            </Button>
-            <Button
-              href='https://github.com/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiGithub className='w-5 text-default-500' />}
-              variant='light'
-            >
-              GitHub
-            </Button>
-            <Button
-              href='https://simpleicons.org/'
-              as={Link}
-              className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
-              startContent={<SiSimpleicons className='w-5 text-default-500' />}
-              variant='light'
-            >
-              Simple Icons
-            </Button>
+            {technicalStack.map(({ name, url, icon }, index) => (
+              <Button
+                key={index}
+                as={Link}
+                href={url}
+                className='col-span-2 max-h-12 w-full object-contain lg:col-span-1'
+                startContent={icon}
+                variant='light'
+              >
+                {name}
+              </Button>
+            ))}
           </div>
         </div>
       </div>
