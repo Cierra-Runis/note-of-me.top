b505a9071a7d0cb6894c43dd8a830a4e0c651a8c
Cierra-Runis <byrdsaron@gmail.com>
2024-12-30 18:36:17 +0800

    refactor: Use Better Color Palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,59 +4,88 @@ import type { Config as TailwindConfig } from 'tailwindcss';
 
 const nextUIPluginConfig: NextUIPluginConfig = {
   themes: {
-    light: {
-      colors: {
-        background: '#FEFEFE',
-        foreground: {
-          DEFAULT: '#6F7A90',
-          50: '#D7DAE0',
-          100: '#CBCFD7',
-          200: '#B4BAC5',
-          300: '#9DA5B4',
-          400: '#868FA2',
-          500: '#6F7A90',
-          600: '#5D6779',
-          700: '#4B5362',
-          800: '#3A404B',
-          900: '#282C34',
-        },
-        default: {
-          foreground: '#6F7A90',
-          50: '#D7DAE0',
-          100: '#CBCFD7',
-          200: '#B4BAC5',
-          300: '#9DA5B4',
-          400: '#868FA2',
-          500: '#6F7A90',
-          600: '#5D6779',
-          700: '#4B5362',
-          800: '#3A404B',
-          900: '#282C34',
-        },
-      },
-    },
     dark: {
       colors: {
+        /// https://uicolors.app/create
+        /// Use #282C34 as seed, then generate these colors
+        /// Since #282C34 be set to 950, the background color
+        /// Then 50-900 will be the foreground colors
+        /// Use the same order as the light mode
+        /// Reverse the order of the colors for dark mode
         background: '#282C34',
         foreground: {
-          DEFAULT: '#868FA2',
-          900: '#D7DAE0',
-          800: '#CBCFD7',
-          700: '#B4BAC5',
-          600: '#9DA5B4',
-          500: '#868FA2',
-          400: '#6F7A90',
-          300: '#5D6779',
-          200: '#4B5362',
-          100: '#3A404B',
-          50: '#282C34',
+          DEFAULT: '#8895a8', /// DEFAULT is the same as 500
+          900: '#f6f7f9',
+          800: '#edeef1',
+          700: '#d6dae1',
+          600: '#b2bac7',
+          500: '#8895a8',
+          400: '#6a798d',
+          300: '#546175',
+          200: '#454f5f',
+          100: '#3c4450',
+          50: '#353b45',
         },
+        /// Default colors as same as foreground
         default: {
-          foreground: '#868FA2',
+          DEFAULT: '#454f5f', /// But DEFAULT is the same as 200
+          900: '#f6f7f9',
+          800: '#edeef1',
+          700: '#d6dae1',
+          600: '#b2bac7',
+          500: '#8895a8',
+          400: '#6a798d',
+          300: '#546175',
+          200: '#454f5f',
+          100: '#3c4450',
+          50: '#353b45',
         },
+        /// Content colors use 50-300 from default (as same as foreground)
+        content4: '#546175',
+        content3: '#454f5f',
+        content2: '#3c4450',
+        content1: '#353b45',
+      },
+    },
+    light: {
+      colors: {
+        background: '#FFFFFF', /// Generator doesn't generate this one, so use white
+        foreground: {
+          DEFAULT: '#454f5f', /// DEFAULT is the same as 700
+          50: '#f6f7f9',
+          100: '#edeef1',
+          200: '#d6dae1',
+          300: '#b2bac7',
+          400: '#8895a8',
+          500: '#6a798d',
+          600: '#546175',
+          700: '#454f5f',
+          800: '#3c4450',
+          900: '#353b45',
+        },
+        /// Default colors as same as foreground
+        default: {
+          DEFAULT: '#6a798d', /// But DEFAULT is the same as 500
+          50: '#f6f7f9',
+          100: '#edeef1',
+          200: '#d6dae1',
+          300: '#b2bac7',
+          400: '#8895a8',
+          500: '#6a798d',
+          600: '#546175',
+          700: '#454f5f',
+          800: '#3c4450',
+          900: '#353b45',
+        },
+        /// Content colors use 50-300 from default (as same as foreground)
+        content1: '#f6f7f9',
+        content2: '#edeef1',
+        content3: '#d6dae1',
+        content4: '#b2bac7',
       },
     },
   },
+  // },
 };
 
 const tailwindConfig: TailwindConfig = {
