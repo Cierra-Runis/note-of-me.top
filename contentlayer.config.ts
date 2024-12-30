e37ac1376990a13b06931f5a2913d8097a3e0c71
Cierra-Runis <byrdsaron@gmail.com>
2024-12-30 21:22:39 +0800

    feat: Add ids To Headings by rehype-slug

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,12 +1,25 @@
-import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
+import {
+  ComputedFields,
+  defineDocumentType,
+  makeSource,
+} from 'contentlayer2/source-files';
 import { KatexOptions } from 'katex';
 import rehypeKaTeX from 'rehype-katex';
 import {
   rehypePrettyCode,
   Options as RehypePrettyCodeOptions,
 } from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
 import remarkGFM, { Options as RemarkGFMOptions } from 'remark-gfm';
 import remarkMath from 'remark-math';
+
+const computedFields: ComputedFields = {
+  url: {
+    type: 'string',
+    resolve: (post: { _raw: { flattenedPath: string } }) =>
+      `/post/${post._raw.flattenedPath}`,
+  },
+};
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -16,13 +29,7 @@ export const Post = defineDocumentType(() => ({
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
   },
-  computedFields: {
-    url: {
-      type: 'string',
-      resolve: (post: { _raw: { flattenedPath: string } }) =>
-        `/post/${post._raw.flattenedPath}`,
-    },
-  },
+  computedFields,
 }));
 
 const rehypeKaTeXOptions: KatexOptions = {
@@ -50,6 +57,8 @@ export default makeSource({
       [rehypeKaTeX, rehypeKaTeXOptions],
       /// https://rehype-pretty.pages.dev
       [rehypePrettyCode, rehypePrettyCodeOptions],
+      /// https://github.com/rehypejs/rehype-slug
+      [rehypeSlug],
     ],
     remarkPlugins: [
       /// https://github.com/remarkjs/remark-math
