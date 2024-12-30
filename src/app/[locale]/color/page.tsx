083d09c895ab1eb760ac0708640759611dd65708
Cierra-Runis <byrdsaron@gmail.com>
2024-12-30 18:32:01 +0800

    refactor: Use Better Design for Color Page

--- src/app/[locale]/color/page.tsx
+++ src/app/[locale]/color/page.tsx
@@ -2,100 +2,121 @@ export default function ColorPage() {
   return (
     <div>
       <h1>Color Page</h1>
-      <h2>Layout</h2>
-      <div className='bg-background p-4'>Background</div>
-      <div className='bg-foreground p-4'>Foreground</div>
-      <div className='bg-divider p-4'>Divider</div>
-      <div className='bg-focus p-4'>Focus</div>
-      <h2>Foreground</h2>
-      <div className='bg-foreground-50 p-4'>50</div>
-      <div className='bg-foreground-100 p-4'>100</div>
-      <div className='bg-foreground-200 p-4'>200</div>
-      <div className='bg-foreground-300 p-4'>300</div>
-      <div className='bg-foreground-400 p-4'>400</div>
-      <div className='bg-foreground-500 p-4'>500</div>
-      <div className='bg-foreground-600 p-4'>600</div>
-      <div className='bg-foreground-700 p-4'>700</div>
-      <div className='bg-foreground-800 p-4'>800</div>
-      <div className='bg-foreground-900 p-4'>900</div>
-      <h2>Content</h2>
-      <div className='bg-content1 p-4'>Content 1</div>
-      <div className='bg-content2 p-4'>Content 2</div>
-      <div className='bg-content3 p-4'>Content 3</div>
-      <div className='bg-content4 p-4'>Content 4</div>
-      <h2>Base</h2>
-      <div className='bg-default p-4'>Default</div>
-      <div className='bg-primary p-4'>Primary</div>
-      <div className='bg-secondary p-4'>Secondary</div>
-      <div className='bg-success p-4'>Success</div>
-      <div className='bg-warning p-4'>Warning</div>
-      <div className='bg-danger p-4'>Danger</div>
-      <h2>Default</h2>
-      <div className='bg-default-50 p-4'>50</div>
-      <div className='bg-default-100 p-4'>100</div>
-      <div className='bg-default-200 p-4'>200</div>
-      <div className='bg-default-300 p-4'>300</div>
-      <div className='bg-default-400 p-4'>400</div>
-      <div className='bg-default-500 p-4'>500</div>
-      <div className='bg-default-600 p-4'>600</div>
-      <div className='bg-default-700 p-4'>700</div>
-      <div className='bg-default-800 p-4'>800</div>
-      <div className='bg-default-900 p-4'>900</div>
-      <h2>Primary</h2>
-      <div className='bg-primary-50 p-4'>50</div>
-      <div className='bg-primary-100 p-4'>100</div>
-      <div className='bg-primary-200 p-4'>200</div>
-      <div className='bg-primary-300 p-4'>300</div>
-      <div className='bg-primary-400 p-4'>400</div>
-      <div className='bg-primary-500 p-4'>500</div>
-      <div className='bg-primary-600 p-4'>600</div>
-      <div className='bg-primary-700 p-4'>700</div>
-      <div className='bg-primary-800 p-4'>800</div>
-      <div className='bg-primary-900 p-4'>900</div>
-      <h2>Secondary</h2>
-      <div className='bg-secondary-50 p-4'>50</div>
-      <div className='bg-secondary-100 p-4'>100</div>
-      <div className='bg-secondary-200 p-4'>200</div>
-      <div className='bg-secondary-300 p-4'>300</div>
-      <div className='bg-secondary-400 p-4'>400</div>
-      <div className='bg-secondary-500 p-4'>500</div>
-      <div className='bg-secondary-600 p-4'>600</div>
-      <div className='bg-secondary-700 p-4'>700</div>
-      <div className='bg-secondary-800 p-4'>800</div>
-      <div className='bg-secondary-900 p-4'>900</div>
-      <h2>Success</h2>
-      <div className='bg-success-50 p-4'>50</div>
-      <div className='bg-success-100 p-4'>100</div>
-      <div className='bg-success-200 p-4'>200</div>
-      <div className='bg-success-300 p-4'>300</div>
-      <div className='bg-success-400 p-4'>400</div>
-      <div className='bg-success-500 p-4'>500</div>
-      <div className='bg-success-600 p-4'>600</div>
-      <div className='bg-success-700 p-4'>700</div>
-      <div className='bg-success-800 p-4'>800</div>
-      <div className='bg-success-900 p-4'>900</div>
-      <h2>Warning</h2>
-      <div className='bg-warning-50 p-4'>50</div>
-      <div className='bg-warning-100 p-4'>100</div>
-      <div className='bg-warning-200 p-4'>200</div>
-      <div className='bg-warning-300 p-4'>300</div>
-      <div className='bg-warning-400 p-4'>400</div>
-      <div className='bg-warning-500 p-4'>500</div>
-      <div className='bg-warning-600 p-4'>600</div>
-      <div className='bg-warning-700 p-4'>700</div>
-      <div className='bg-warning-800 p-4'>800</div>
-      <div className='bg-warning-900 p-4'>900</div>
-      <h2>Danger</h2>
-      <div className='bg-danger-50 p-4'>50</div>
-      <div className='bg-danger-100 p-4'>100</div>
-      <div className='bg-danger-200 p-4'>200</div>
-      <div className='bg-danger-300 p-4'>300</div>
-      <div className='bg-danger-400 p-4'>400</div>
-      <div className='bg-danger-500 p-4'>500</div>
-      <div className='bg-danger-600 p-4'>600</div>
-      <div className='bg-danger-700 p-4'>700</div>
-      <div className='bg-danger-800 p-4'>800</div>
-      <div className='bg-danger-900 p-4'>900</div>
+
+      <div className='flex'>
+        <div>
+          <h2>Layout</h2>
+          <div className='p-4'>-</div>
+          <div className='bg-background p-4'>Background</div>
+          <div className='bg-divider p-4'>Divider</div>
+          <div className='bg-focus p-4'>Focus</div>
+        </div>
+        <div>
+          <h2>Foreground</h2>
+          <div className='bg-foreground p-4'>Foreground</div>
+          <div className='bg-foreground-50 p-4'>50</div>
+          <div className='bg-foreground-100 p-4'>100</div>
+          <div className='bg-foreground-200 p-4'>200</div>
+          <div className='bg-foreground-300 p-4'>300</div>
+          <div className='bg-foreground-400 p-4'>400</div>
+          <div className='bg-foreground-500 p-4'>500</div>
+          <div className='bg-foreground-600 p-4'>600</div>
+          <div className='bg-foreground-700 p-4'>700</div>
+          <div className='bg-foreground-800 p-4'>800</div>
+          <div className='bg-foreground-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Default</h2>
+          <div className='bg-default p-4'>Default</div>
+          <div className='bg-default-50 p-4'>50</div>
+          <div className='bg-default-100 p-4'>100</div>
+          <div className='bg-default-200 p-4'>200</div>
+          <div className='bg-default-300 p-4'>300</div>
+          <div className='bg-default-400 p-4'>400</div>
+          <div className='bg-default-500 p-4'>500</div>
+          <div className='bg-default-600 p-4'>600</div>
+          <div className='bg-default-700 p-4'>700</div>
+          <div className='bg-default-800 p-4'>800</div>
+          <div className='bg-default-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Content</h2>
+          <div className='p-4'>-</div>
+          <div className='bg-content1 p-4'>Content 1</div>
+          <div className='bg-content2 p-4'>Content 2</div>
+          <div className='bg-content3 p-4'>Content 3</div>
+          <div className='bg-content4 p-4'>Content 4</div>
+        </div>
+        <div>
+          <h2>Primary</h2>
+          <div className='bg-primary p-4'>Primary</div>
+          <div className='bg-primary-50 p-4'>50</div>
+          <div className='bg-primary-100 p-4'>100</div>
+          <div className='bg-primary-200 p-4'>200</div>
+          <div className='bg-primary-300 p-4'>300</div>
+          <div className='bg-primary-400 p-4'>400</div>
+          <div className='bg-primary-500 p-4'>500</div>
+          <div className='bg-primary-600 p-4'>600</div>
+          <div className='bg-primary-700 p-4'>700</div>
+          <div className='bg-primary-800 p-4'>800</div>
+          <div className='bg-primary-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Secondary</h2>
+          <div className='bg-secondary p-4'>Secondary</div>
+          <div className='bg-secondary-50 p-4'>50</div>
+          <div className='bg-secondary-100 p-4'>100</div>
+          <div className='bg-secondary-200 p-4'>200</div>
+          <div className='bg-secondary-300 p-4'>300</div>
+          <div className='bg-secondary-400 p-4'>400</div>
+          <div className='bg-secondary-500 p-4'>500</div>
+          <div className='bg-secondary-600 p-4'>600</div>
+          <div className='bg-secondary-700 p-4'>700</div>
+          <div className='bg-secondary-800 p-4'>800</div>
+          <div className='bg-secondary-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Success</h2> <div className='bg-success p-4'>Success</div>
+          <div className='bg-success-50 p-4'>50</div>
+          <div className='bg-success-100 p-4'>100</div>
+          <div className='bg-success-200 p-4'>200</div>
+          <div className='bg-success-300 p-4'>300</div>
+          <div className='bg-success-400 p-4'>400</div>
+          <div className='bg-success-500 p-4'>500</div>
+          <div className='bg-success-600 p-4'>600</div>
+          <div className='bg-success-700 p-4'>700</div>
+          <div className='bg-success-800 p-4'>800</div>
+          <div className='bg-success-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Warning</h2>
+          <div className='bg-warning p-4'>Warning</div>
+          <div className='bg-warning-50 p-4'>50</div>
+          <div className='bg-warning-100 p-4'>100</div>
+          <div className='bg-warning-200 p-4'>200</div>
+          <div className='bg-warning-300 p-4'>300</div>
+          <div className='bg-warning-400 p-4'>400</div>
+          <div className='bg-warning-500 p-4'>500</div>
+          <div className='bg-warning-600 p-4'>600</div>
+          <div className='bg-warning-700 p-4'>700</div>
+          <div className='bg-warning-800 p-4'>800</div>
+          <div className='bg-warning-900 p-4'>900</div>
+        </div>
+        <div>
+          <h2>Danger</h2>
+          <div className='bg-danger p-4'>Danger</div>
+          <div className='bg-danger-50 p-4'>50</div>
+          <div className='bg-danger-100 p-4'>100</div>
+          <div className='bg-danger-200 p-4'>200</div>
+          <div className='bg-danger-300 p-4'>300</div>
+          <div className='bg-danger-400 p-4'>400</div>
+          <div className='bg-danger-500 p-4'>500</div>
+          <div className='bg-danger-600 p-4'>600</div>
+          <div className='bg-danger-700 p-4'>700</div>
+          <div className='bg-danger-800 p-4'>800</div>
+          <div className='bg-danger-900 p-4'>900</div>
+        </div>
+      </div>
     </div>
   );
 }
