c7ced3af01bddb86e7c704760fc0633d8ddc8cfe
Cierra-Runis <byrdsaron@gmail.com>
2025-01-01 22:54:40 +0800

    refactor: Remove Calendar in Home Page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,9 @@
-import { Calendar } from '@nextui-org/calendar';
-
 export default function Home() {
   return (
     <section className='flex h-full flex-col items-center justify-center gap-4 py-8 md:py-10'>
-      <Calendar></Calendar>
+      <h1 className='text-center text-4xl font-bold'>
+        Welcome to ノート of 我
+      </h1>
     </section>
   );
 }
