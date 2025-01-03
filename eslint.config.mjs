ff839b0ca29ca63cd32f349fcb59bef8f71341d3
Cierra-Runis <byrdsaron@gmail.com>
2025-01-03 23:24:44 +0800

    fix: Use ESLinter to Sorting Keys & Imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
+/// [eslint-config-prettier](https://github.com/prettier/eslint-config-prettier)
+/// Turns off all rules that are unnecessary or might conflict with Prettier
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPerfectionist from 'eslint-plugin-perfectionist';
+/// While you can use the official plugin [prettier-plugin-tailwindcss](https://www.npmjs.com/package/prettier-plugin-tailwindcss) for ordering your classnames...
+/// eslint-plugin-tailwindcss offers more than 5 other rules, that you can benefit from on top of prettier-plugin-tailwindcss.
+import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
-const configs = [...compat.extends('next', 'prettier'), js.configs.recommended];
+const eslintConfig = [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintPluginPerfectionist.configs['recommended-alphabetical'],
+  eslintConfigPrettier,
+  ...eslintPluginTailwindCSS.configs['flat/recommended'],
+];
 
-export default configs;
+export default eslintConfig;
