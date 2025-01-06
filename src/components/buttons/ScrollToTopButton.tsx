481b24c965926a40b8b49a1dc92eb43c5f30f8b0
Cierra-Runis <byrdsaron@gmail.com>
2025-01-07 00:24:43 +0800

    feat: Add Fade Animation For `ScrollToTopButton`

--- src/components/buttons/ScrollToTopButton.tsx
+++ src/components/buttons/ScrollToTopButton.tsx
@@ -2,37 +2,33 @@
 
 import { ArrowUpIcon } from '@heroicons/react/24/outline';
 import { Button } from '@nextui-org/button';
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
 export default function ScrollToTopButton() {
-  const [isShow, setIsShow] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setIsShow(true);
-      } else {
-        setIsShow(false);
-      }
-    };
-
+    const handleScroll = () => setShowButton(window.scrollY > 25);
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const handleClick = () => {
-    window.scrollTo({ behavior: 'smooth', top: 0 });
-  };
+  const scrollToTop = () => window.scrollTo({ behavior: 'smooth', top: 0 });
 
   return (
-    <Button
-      className={`fixed bottom-12 right-12 ${isShow ? 'flex' : 'hidden'}`}
-      isIconOnly
-      onPress={handleClick}
-      size='sm'
-      variant='ghost'
+    <motion.div
+      animate={{
+        opacity: showButton ? 1 : 0,
+        visibility: showButton ? 'visible' : 'hidden',
+      }}
+      className='fixed bottom-12 right-6'
+      initial={{ opacity: 0, visibility: 'hidden' }}
+      transition={{ duration: 0.3 }}
     >
-      <ArrowUpIcon className='w-5 text-foreground-500' />
-    </Button>
+      <Button isIconOnly onPress={scrollToTop} size='sm' variant='ghost'>
+        <ArrowUpIcon className='w-5' />
+      </Button>
+    </motion.div>
   );
 }
