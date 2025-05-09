9e3c46d5252feb785ccffb37c4e5ed0b13d2365e
Cierra-Runis <byrdsaron@gmail.com>
2025-05-10 00:01:45 +0800

    fix: Fix Theme Losing for Exiting 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,7 @@
-import BaseLayout from '@/components/roots/BaseLayout';
 import NotFoundPage from '@/components/roots/NotFoundPage';
 
 // This page renders when a route like `/unknown.txt` is requested.
 // In this case, the layout at `app/layout.tsx` receives.
 export default function GlobalNotFound() {
-  return (
-    <BaseLayout>
-      <NotFoundPage />
-    </BaseLayout>
-  );
+  return <NotFoundPage />;
 }
