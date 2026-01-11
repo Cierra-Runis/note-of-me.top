16bb6df3fabb93255e2718e82971f4bf538aa11e
Cierra-Runis <byrdsaron@gmail.com>
2026-01-12 02:17:46 +0900

    refactor(PostCard): reorganize imports and move badges to CardFooter

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,7 +5,13 @@ import { parseISO } from 'date-fns';
 import NextLink from 'next/link';
 
 import { Badge } from '@/components/ui/badge';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function PostCard(post: Post) {
   return (
@@ -19,15 +25,17 @@ export default function PostCard(post: Post) {
         <CardHeader>
           <CardTitle className='text-lg'>{post.title}</CardTitle>
         </CardHeader>
-        <CardContent>
-          <div className='mb-3 flex flex-wrap gap-1 text-xs'>
-            <Badge>{parseISO(post.date).toUTCString()}</Badge>
-            <Badge>{`${post.wordCount} 字`}</Badge>
-          </div>
+        <CardContent className='h-full'>
           <p className='line-clamp-3 text-sm break-all text-muted-foreground'>
             {post.body.raw}
           </p>
         </CardContent>
+        <CardFooter className='flex flex-wrap justify-between gap-1'>
+          <Badge variant='outline'>
+            {parseISO(post.date).toLocaleString()}
+          </Badge>
+          <Badge variant='outline'>{`${post.wordCount} 字`}</Badge>
+        </CardFooter>
       </Card>
     </NextLink>
   );
